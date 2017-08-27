9473000791ef1401aa1b06a368672ff9832b0b2c
Nirosh Gunaratne <niroshpg@gmail.com>
2017-08-28 05:35:14 +1000

    init commit

---
+++ movietrailer/media.py
@@ -0,0 +1,40 @@
+#===============================================================================
+# 
+# media.py
+# 
+# This module defines the class Movie to create instance
+# of movie objects
+# 
+# @author: Nirosh Gunaratne
+# @since: 28/08/2017
+# @version: 1.0
+# 
+#===============================================================================
+
+class Movie(object):
+    #===========================================================================
+    # This class holds attributes related to instance of a movie
+    #===========================================================================
+
+    def __init__(self,movie_title,movie_storyline,poster_image,trailer_youtube): 
+        #===========================================================================
+        # create movie instance using specified parameters
+        # you tube of the movie trailer 
+        #         
+        # Parameters
+        # ----------
+        # movie_title : String
+        #     title of the movie
+        # movie_storyline : String
+        #     story line of the movie
+        # poster_image : String
+        #     poster image of the movie
+        # trailer_youtube : String
+        #     you tube of the movie trailer   
+        # 
+        #===========================================================================
+        self.title=movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url=poster_image
+        self.trailer_youtube_url=trailer_youtube
+        
---
+++ movietrailer/mediacenter.py
@@ -0,0 +1,61 @@
+#===============================================================================
+# Movie Trailer Web Site
+# ----------------------
+# This project is to track favorite movie trailers with its associate meta data. 
+# 
+# This is the main module of the movie trailer web site. This will create list of 
+# instance of movies using Movie class and launch them in a web page using
+# launcher module
+# 
+# @author: Nirosh Gunaratne
+# @since: 28/08/2017
+# @version: 1.0
+#===============================================================================
+
+from movietrailer import launcher
+from movietrailer import media
+'''
+create  each instance of favorite movies available
+'''
+metrix = media.Movie("Matrix",
+                     "A story about escaping from a virtul matrix created by machines",
+                     "http://static.rogerebert.com/uploads/movie/movie_poster/the-matrix-1999/large_gynBNzwyaHKtXqlEKKLioNkjKgN.jpg",
+                     "https://www.youtube.com/watch?v=vKQi3bBA1y8")
+
+avatar = media.Movie("Avatar",
+                     "A marine on an alien planet",
+                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+
+toystory = media.Movie("Toy Story",
+                     "A stroy of a boy and his toys that comes to life",
+                     "https://lumiere-a.akamaihd.net/v1/images/open-uri20150422-20810-m8zzyx_5670999f.jpeg",
+                     "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+
+nemo = media.Movie("Finding Nemo",
+                     "A clown fish Maline searching his lost son Nemo",
+                     "https://s-media-cache-ak0.pinimg.com/originals/76/e9/bb/76e9bb65c557b261bb2e71a0dc08bd55.jpg",
+                     "https://www.youtube.com/watch?v=2zLkasScy7A&t=15s")
+
+
+dragons = media.Movie("How to train your dragon",
+                     "Stroy about kindom of dragons",
+                     "http://static.rogerebert.com/uploads/movie/movie_poster/how-to-train-your-dragon-2010/large_zMAm3WYmvD40FaWFsOmpicQFabz.jpg",
+                     "https://www.youtube.com/watch?v=oKiYuIsPxYk")
+
+xmen = media.Movie("X-Men",
+                     "A story about genetic super humans",
+                     "https://images-na.ssl-images-amazon.com/images/M/MV5BMjU1ODM1MzYxN15BMl5BanBnXkFtZTgwOTA4NDE2ODE@._V1_UY1200_CR91,0,630,1200_AL_.jpg",
+                     "https://www.youtube.com/watch?v=Iy5R5_T243w")
+'''
+create list of movies
+'''
+movies = [metrix,avatar,toystory,nemo,dragons,xmen]
+
+'''
+pass the created list of movies to launcher module and open them in a web browser
+'''
+launcher.open_movies_page(movies)
+
+
